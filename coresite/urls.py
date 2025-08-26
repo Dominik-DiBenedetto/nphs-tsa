e405a9dcd7ee7d7948eb4af04351a20d28e92f77
Dom <thescriptinglegend@gmail.com>
2025-08-26 06:19:10 -0400

    sponsorpage

--- coresite/urls.py
+++ coresite/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.home, name="index"),
     path("home/", views.home, name="home"),
     path("sponsorships/", views.sponsorships, name="sponsorships"),
+    path("sponsors/", views.sponsors, name="sponsors"),
     path("update_server/", views.update_server, name="update_server"),
 ]
--- coresite/views.py
+++ coresite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import git
 
 # Create your views here.
@@ -9,9 +10,12 @@ def home(request):
 def sponsorships(request):
     return render(request, "sponsorships.html")
 
+def sponsors(request):
+    return render(request, "sponsors.html")
+
 def update_server(request):
     if request.method == "POST":
         repo = git.Repo("https://github.com/Dominik-DiBenedetto/nphs-tsa.git")
         origin = repo.remotes.origin
         origin.pull()
-        return "Success", 200
+        return HttpResponse("Success")
