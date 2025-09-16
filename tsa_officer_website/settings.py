85d57727e61e0fa489171dc6ab15ab9b764f3fb1
Dom <69871335+Dominik-DiBenedetto@users.noreply.github.com>
2025-09-15 22:54:43 -0400

    added whitenoise to serve static files

--- tsa_officer_website/settings.py
+++ tsa_officer_website/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 WEBPUSH_SETTINGS = {
@@ -91,13 +92,6 @@ WSGI_APPLICATION = 'tsa_officer_website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 # DATABASES = {
 #     'default': {
