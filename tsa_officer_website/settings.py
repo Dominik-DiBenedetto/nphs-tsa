a072f73b805082bff3cad89690f324a66e79774d
Dom <69871335+Dominik-DiBenedetto@users.noreply.github.com>
2025-09-15 22:43:32 -0400

    updated database config

--- tsa_officer_website/settings.py
+++ tsa_officer_website/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-import os
+import os, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,18 +99,20 @@ WSGI_APPLICATION = 'tsa_officer_website.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv("DB_NAME"),       # database name
+#         'USER': os.getenv("DB_USER"),     # the user you created
+#         'PASSWORD':os.getenv("DB_PASSWORD"),    # the password you chose
+#         'HOST': 'localhost',
+#         'PORT': '5432',              # default PostgreSQL port
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),       # database name
-        'USER': os.getenv("DB_USER"),     # the user you created
-        'PASSWORD':os.getenv("DB_PASSWORD"),    # the password you chose
-        'HOST': 'localhost',
-        'PORT': '5432',              # default PostgreSQL port
-    }
+    'default': dj_database_url.config(default=os.getenv("DB_URL"))
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
