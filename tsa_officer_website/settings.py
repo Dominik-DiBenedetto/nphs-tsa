6f0204562f1b41f845c041af13427b65aae99a5d
Dom <69871335+Dominik-DiBenedetto@users.noreply.github.com>
2025-09-15 22:21:40 -0400

    updated allowed hosts

--- tsa_officer_website/settings.py
+++ tsa_officer_website/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "nphstsa.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "nphstsa.pythonanywhere.com", "nphs-tsa.onrender.com", "nphs-tsa.com"]
 
 # Application definition
 
