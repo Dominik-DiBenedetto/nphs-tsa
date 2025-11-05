50f7dd62b9e12c8a67476c1e96985371cd522a86
Dom <69871335+Dominik-DiBenedetto@users.noreply.github.com>
2025-11-04 20:21:27 -0500

    Update settings.py
    changed from render db url

--- tsa_officer_website/settings.py
+++ tsa_officer_website/settings.py
@@ -104,6 +104,10 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv("DB_URL"))
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
